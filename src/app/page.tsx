dabb77bd1a1eb8ccb137dd0eaa859883a0ff4ab5
zehraikizler <zehraaikizler@gmail.com>
2023-08-23 16:31:55 +0300

    css grid edited

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,19 +7,19 @@ import Score from "@/components/score";
 export default function Template({ children }: { children: React.ReactNode }) {
   return (
     <div className="game-page bg-white mx-auto py-6 sm:px-6 lg:px-8 grid grid-cols-8">
-      <div className="col-span-2 p-3">
+      <div className="col-span-4 lg:col-span-2 p-3 h-14">
         <Score />
       </div>
-      <div className="col-span-4 p-3 flex flex-col">
-        <div className="border rounded-xl h-60 p-8 overflow-y-scroll no-scrollbar flex flex-col">
+      <div className="col-span-4 lg:col-span-2 lg:order-last p-3 h-14">
+        <Count />
+      </div>
+      <div className="col-span-8 lg:col-span-4 p-3 flex flex-col">
+        <div className="border rounded-xl h-80 lg:h-60 p-8 overflow-y-scroll no-scrollbar flex flex-col">
           <Answers />
         </div>
         <div className="mt-auto">
           <GamingBox />
         </div>
-      </div>
-      <div className="col-span-2 p-3">
-        <Count />
       </div>
     </div>
   );
--- src/components/count/index.tsx
+++ src/components/count/index.tsx
@@ -5,7 +5,7 @@ const Count = () => {
   const [count, setCount] = useState(10)
 
   return (
-    <div className='w-100 flex justify-center text-4xl text-green-600 font-extrabold my-5'>{count}</div>
+    <div className='w-100 flex justify-center text-3xl text-green-600 font-extrabold my-5'>{count}</div>
   )
 }
 
